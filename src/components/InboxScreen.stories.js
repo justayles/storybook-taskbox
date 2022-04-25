2b44b6d87889f638ba80e4dacd7039adc65f880b
Justin Ayles <justayles@gmail.com>
2022-04-25 15:53:52 +0100

    reverse action

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -44,7 +44,7 @@ Default.play = async ({ canvasElement }) => {
     // Simulates pinning the first task
     await fireEvent.click(canvas.getByLabelText('pinTask-1'));
     // Simulates pinning the third task
-    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+    // await fireEvent.click(canvas.getByLabelText('pinTask-3'));
   });
 };
 
