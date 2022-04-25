fcd684307774bb66444bb60b9b28ae77cf235c66
Justin Ayles <justayles@gmail.com>
2022-04-25 14:41:05 +0100

    init commit

---
+++ src/components/Task.stories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Task from './Task';
+
+export default {
+  component: Task,
+  title: 'Task',
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Default Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2021, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    title: 'Test Pinned Task',
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    title: 'Test Archived Task',
+    state: 'TASK_ARCHIVED',
+  },
+};
